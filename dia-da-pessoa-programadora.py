01fd1ccb5895259f4fb7d7377a40820f94b6f079
Ana Carolina Castro <ana.castro@corp.globo.com>
2021-09-16 13:56:52 -0300

    Add script

---
+++ dia-da-pessoa-programadora.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+def type_print(phrase):
+  for i in phrase: 
+    print(i, end='', flush=True)
+    time.sleep(0.1)
+
+type_print("Feliz dia da pessoa programadora!\n")
+
+print(
+  '''
+
+
+        )  (
+     (   ) )
+      ) ( (
+    _________
+ .-'---------|  
+( C|/\/\/\/\/|
+ '-./\/\/\/\/|
+   '_________'
+    '-------'
+  '''
+)
